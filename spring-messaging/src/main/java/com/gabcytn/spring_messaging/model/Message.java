4a5aaa9036788c7d1df12cb0c6d16bce7249cb16
gabcytn <cayetanogabriel03@gmail.com>
2024-12-25 22:06:55 +0800

    Feat: message model

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/model/Message.java
@@ -0,0 +1,29 @@
+package com.gabcytn.spring_messaging.model;
+
+public class Message {
+  private String message;
+  private String sender;
+
+  public Message () {}
+
+  public Message(String message, String sender) {
+    this.message = message;
+    this.sender = sender;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getSender() {
+    return sender;
+  }
+
+  public void setSender(String sender) {
+    this.sender = sender;
+  }
+}
