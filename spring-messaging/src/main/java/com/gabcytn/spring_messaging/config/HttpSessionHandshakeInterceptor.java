d28d99d82c100443e5ede3b5fc8023a8c374ebd6
gabcytn <cayetanogabriel03@gmail.com>
2024-12-25 22:05:54 +0800

    Feat: custom handshake interceptor
    Allows websocket to access uuid & username from http sessions

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/config/HttpSessionHandshakeInterceptor.java
@@ -0,0 +1,31 @@
+package com.gabcytn.spring_messaging.config;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class HttpSessionHandshakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            HttpSession session = servletRequest.getServletRequest().getSession();
+            final UUID storedUuid = UUID.fromString((String) session.getAttribute("uuid"));
+            final String username = (String) session.getAttribute("username");
+
+            attributes.put("uuid", storedUuid.toString());
+            attributes.put("username", username);
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
