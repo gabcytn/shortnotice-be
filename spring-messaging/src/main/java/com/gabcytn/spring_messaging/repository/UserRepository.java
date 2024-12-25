094b8b97c1b9cf4da3ab53c6600ad9fd8c24b0f1
gabcytn <cayetanogabriel03@gmail.com>
2024-12-25 12:35:00 +0800

    Feat: core sql queries for auth

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/repository/UserRepository.java
@@ -0,0 +1,49 @@
+package com.gabcytn.spring_messaging.repository;
+
+import com.gabcytn.spring_messaging.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Optional<User> findByUsername (String username) {
+        String sqlQuery = """
+                SELECT *
+                FROM users
+                WHERE username = ?
+                """;
+        return Optional.ofNullable(jdbcTemplate.query(sqlQuery, userResultSetExtractor(), username));
+    }
+
+    public void save (User user) {
+        String sqlQuery = """
+                INSERT INTO users (id, username, password)
+                VALUES (?, ?, ?)
+                """;
+        jdbcTemplate.update(sqlQuery, user.getId(), user.getUsername(), user.getPassword());
+    }
+
+    private ResultSetExtractor<User> userResultSetExtractor () {
+        return rs -> {
+            if (rs.next()) {
+                User user = new User();
+                user.setId(UUID.fromString(rs.getString("id")));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(rs.getString("role"));
+                return user;
+            }
+            return null;
+        };
+    }
+}
