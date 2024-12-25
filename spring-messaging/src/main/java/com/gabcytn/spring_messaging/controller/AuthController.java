a6b48e6584f5c486109cfaff959029a4b049de69
gabcytn <cayetanogabriel03@gmail.com>
2024-12-25 12:33:51 +0800

    Feat: basic controller for auth

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.gabcytn.spring_messaging.controller;
+
+import com.gabcytn.spring_messaging.model.User;
+import com.gabcytn.spring_messaging.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController (AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login (
+            @RequestBody User user,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        return authService.authenticate(user, request, response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register (@RequestBody User user) {
+        return authService.register(user);
+    }
+
+    @GetMapping("/session")
+    public String sessionId (HttpServletRequest request) {
+        return request.getSession().getId();
+    }
+}
