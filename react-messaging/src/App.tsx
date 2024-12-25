0b8e3f3bdc2dd9f60083e72f4bcfa99ada6018d7
gabcytn <cayetanogabriel03@gmail.com>
2024-12-25 19:00:38 +0800

    Feat: real-time messaging (in console)

--- react-messaging/src/App.tsx
+++ react-messaging/src/App.tsx
@@ -1,7 +1,67 @@
+import { useNavigate } from "react-router";
+import { handleLogout } from "./pages/auth/functions";
+import { useEffect, useState } from "react";
+import { Client } from "@stomp/stompjs";
+
+const WEBSOCKET_URL = import.meta.env.VITE_WEBSOCKET_URL;
+const stompClient = new Client({
+  brokerURL: `${WEBSOCKET_URL}/short-notice`,
+  onConnect: () => {
+    stompClient.subscribe("/topic/message", (message) => {
+      const messageFromServer = JSON.parse(message.body);
+      console.log(messageFromServer.message);
+    });
+  },
+});
+
+stompClient.onStompError = (frame) => {
+  console.error("Broker reported error: " + frame.headers["message"]);
+  console.error("Additional details: " + frame.body);
+};
+stompClient.onWebSocketError = (error) => {
+  console.error("Error with websocket", error);
+};
+
 function App() {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    stompClient.activate();
+    console.log("client activated");
+    return () => {
+      stompClient.deactivate();
+    };
+  }, []);
   return (
-    <div>
-      <h2>Messaging app</h2>
+    <div className="container">
+      <h2 className="text-center">Messaging app</h2>
+      <input
+        className="form-control"
+        type="text"
+        value={message}
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          stompClient.publish({
+            destination: "/app/test",
+            body: JSON.stringify({ message: message }),
+          });
+        }}
+      >
+        Send
+      </button>
+      <button
+        className="btn btn-danger mt-3"
+        onClick={() => {
+          handleLogout(navigate);
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 }
