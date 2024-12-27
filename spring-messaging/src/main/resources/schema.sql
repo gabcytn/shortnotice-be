1908a09c54f0e3d2c24647084a1196c467a7a5ae
Gabriel Cayetano <cayetanogabriel03@gmail.com>
2024-12-27 16:26:09 +0800

    Update schema.sql

--- spring-messaging/src/main/resources/schema.sql
+++ spring-messaging/src/main/resources/schema.sql
@@ -8,30 +8,30 @@ CREATE TABLE users (
 );
 
 CREATE TABLE channels (
-	id BIGINT NOT NULL,
-	name VARCHAR(255) NOT NULL,
+	id SERIAL NOT NULL,
+	type VARCHAR(7) NOT NULL,
 	CONSTRAINT channels_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE subscribers (
-	channel BIGINT NOT NULL,
+	channel_id INT NOT NULL,
 	user_id UUID NOT NULL,
-	CONSTRAINT subscribers_ak UNIQUE (channel, user_id),
+	CONSTRAINT subscribers_ak UNIQUE (channel_id, user_id),
 	CONSTRAINT subsribers_user_fk FOREIGN KEY (user_id) REFERENCES users(id) 
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT subscribers_channel_fk FOREIGN KEY (channel) REFERENCES channels(id)
+	CONSTRAINT subscribers_channel_fk FOREIGN KEY (channel_id) REFERENCES channels(id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE messages (
-	id BIGINT NOT NULL,
+	id SERIAL NOT NULL,
 	user_id UUID NOT NULL,
-	channel BIGINT NOT NULL,
+	channel_id INT NOT NULL,
 	message TEXT NOT NULL,
 	sent_at TIMESTAMP DEFAULT NOW(),
 	CONSTRAINT messages_pk PRIMARY KEY (id),
 	CONSTRAINT messages_user_fk FOREIGN KEY (user_id) REFERENCES users(id)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT messages_channel_fk FOREIGN KEY (channel) REFERENCES channels(id)
+	CONSTRAINT messages_channel_fk FOREIGN KEY (channel_id) REFERENCES channels(id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
