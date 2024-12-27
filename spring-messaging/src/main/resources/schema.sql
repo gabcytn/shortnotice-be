7efd42be41f1cb12c49885c06f170818654a4d22
gabcytn <cayetanogabriel03@gmail.com>
2024-12-27 19:35:47 +0800

    Update schema.sql

--- spring-messaging/src/main/resources/schema.sql
+++ spring-messaging/src/main/resources/schema.sql
@@ -7,31 +7,37 @@ CREATE TABLE users (
 	CONSTRAINT users_ak UNIQUE (username)
 );
 
-CREATE TABLE channels (
+CREATE TABLE groups (
 	id SERIAL NOT NULL,
-	type VARCHAR(7) NOT NULL,
-	CONSTRAINT channels_pk PRIMARY KEY (id)
+	CONSTRAINT groups_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE subscribers (
-	channel_id INT NOT NULL,
+CREATE TABLE group_members (
+	id INT NOT NULL,
 	user_id UUID NOT NULL,
-	CONSTRAINT subscribers_ak UNIQUE (channel_id, user_id),
-	CONSTRAINT subsribers_user_fk FOREIGN KEY (user_id) REFERENCES users(id) 
+	CONSTRAINT group_members_pk PRIMARY KEY (id, user_id),
+	CONSTRAINT group_members_id_fk FOREIGN KEY(id) REFERENCES groups(id)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT subscribers_channel_fk FOREIGN KEY (channel_id) REFERENCES channels(id)
+	CONSTRAINT group_members_user_id_fk FOREIGN KEY(user_id) REFERENCES users(id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE messages (
 	id SERIAL NOT NULL,
-	user_id UUID NOT NULL,
-	channel_id INT NOT NULL,
+	sender_id UUID NOT NULL,
+	receiver_id UUID,
+	group_id INT,
 	message TEXT NOT NULL,
 	sent_at TIMESTAMP DEFAULT NOW(),
 	CONSTRAINT messages_pk PRIMARY KEY (id),
-	CONSTRAINT messages_user_fk FOREIGN KEY (user_id) REFERENCES users(id)
+	CONSTRAINT messages_sender_fk FOREIGN KEY (sender_id) REFERENCES users(id)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT messages_channel_fk FOREIGN KEY (channel_id) REFERENCES channels(id)
-	ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT messages_receiver_fk FOREIGN KEY (receiver_id) REFERENCES users(id)
+  	ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT messages_group_fk FOREIGN KEY(group_id) REFERENCES groups(id)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT message_destination_chk CHECK (
+		(receiver_id IS NULL AND group_id IS NOT NULL) OR
+		(receiver_id IS NOT NULL AND group_id IS NULL)
+	)
 );
