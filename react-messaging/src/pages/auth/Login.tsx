6acc3fe82191bd166b4608c4d78e258167f635ec
gabcytn <cayetanogabriel03@gmail.com>
2024-12-28 20:42:42 +0800

    Refactor: use reusable components

--- react-messaging/src/pages/auth/Login.tsx
+++ react-messaging/src/pages/auth/Login.tsx
@@ -1,49 +1,45 @@
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router";
 import { handleLoginSubmit } from "./functions";
+import Button from "../../components/Button";
+import FormInput from "../../components/FormInput";
 
 function Login() {
   const navigate = useNavigate();
   const handleSubmit = async (e: FormEvent) => {
-    handleLoginSubmit(e, username, password, navigate);
+    handleLoginSubmit(e, username, password, setIsLoading, navigate);
   };
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
           <h3 className="text-center">Login Page</h3>
           <form onSubmit={handleSubmit}>
-            <div className="form">
-              <label htmlFor="" className="form-label">
-                Username
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="" className="form-label">
-                Password
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-            </div>
-            <button type="submit" className="btn btn-primary mt-3">
-              Submit
-            </button>
+            <FormInput
+              label="Username"
+              id="username"
+              type="text"
+              value={username}
+              setValue={setUsername}
+              disabled={isLoading}
+            />
+            <FormInput
+              label="Password"
+              id="password"
+              type="password"
+              value={password}
+              setValue={setPassword}
+              disabled={isLoading}
+            />
+            <Button
+              title="Submit"
+              type="submit"
+              className="btn-primary mt-3"
+              disabled={isLoading}
+            />
           </form>
         </div>
       </div>
