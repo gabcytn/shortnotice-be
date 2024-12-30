ff23cda327cb8589792b6ef96738aa4f9f4df569
gabcytn <cayetanogabriel03@gmail.com>
2024-12-30 21:21:10 +0800

    Refactor: error handling and type safety

--- react-messaging/src/functions.tsx
+++ react-messaging/src/functions.tsx
@@ -1,16 +1,23 @@
-import { Client } from "@stomp/stompjs";
+import { Client, Message } from "@stomp/stompjs";
 
-export async function subscribeToStomp(stompClient: Client) {
-  const server = import.meta.env.VITE_SERVER_URL;
-  const res = await fetch(`${server}/username`, {
-    credentials: "include",
-  });
-  const username = await res.text();
-  stompClient.onConnect = () => {
-    stompClient.subscribe(`/topic/private/${username}`, (message) => {
-      const messageFromServer = JSON.parse(message.body);
-      console.log(messageFromServer);
+export async function subscribeToStomp(
+  stompClient: Client,
+  callback: (message: Message) => void,
+) {
+  try {
+    const server = import.meta.env.VITE_SERVER_URL;
+    const res = await fetch(`${server}/username`, {
+      credentials: "include",
     });
-  };
-  stompClient.activate();
+    const username = await res.text();
+    stompClient.onConnect = () => {
+      stompClient.subscribe(`/topic/private/${username}`, callback);
+    };
+    stompClient.activate();
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error("Error subscribing");
+      console.error(e.message);
+    }
+  }
 }
