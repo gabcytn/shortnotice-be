b80a78739e8489b561fef454a005686abc94f5e1
gabcytn <cayetanogabriel03@gmail.com>
2024-12-31 14:04:51 +0800

    Feat: map incoming new message requests

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/websocket_controller/MessageController.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/websocket_controller/MessageController.java
@@ -1,15 +1,17 @@
 package com.gabcytn.spring_messaging.websocket_controller;
 
 import com.gabcytn.spring_messaging.model.Message;
-import com.gabcytn.spring_messaging.model.PrivateMessageView;
+import com.gabcytn.spring_messaging.model.PrivateMessage;
 import com.gabcytn.spring_messaging.service.MessageService;
 
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class MessageController {
@@ -22,36 +24,15 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @MessageMapping("/test")
-    @SendTo("/topic/message")
-    public Message message(Message message, SimpMessageHeaderAccessor headerAccessor) throws InterruptedException {
-        final Message messageToBeSent = new Message();
-        final String sender = (String) headerAccessor.getSessionAttributes().get("username");
-
-        messageToBeSent.setMessage(message.getMessage());
-        messageToBeSent.setSender(sender);
-
-        return messageToBeSent;
-    }
-
-    @MessageMapping("/private/{username}")
-    public void testPrivate(
-            @DestinationVariable String username,
+    @MessageMapping("/request/{uuid}")
+    public void privateMessageRequest(
+            @DestinationVariable UUID uuid,
             Message messageReceived,
             SimpMessageHeaderAccessor headerAccessor
     ) {
-        final PrivateMessageView messageToSend = messageService
-                .savePrivateMessage(headerAccessor, messageReceived, username);
-        messagingTemplate.convertAndSend("/topic/private/" + username, messageToSend);
+        final Optional<PrivateMessage> privateMessageView =
+                messageService.createMessageRequest(headerAccessor, messageReceived, uuid);
+        messagingTemplate.convertAndSend("/topic/private/" + uuid, privateMessageView.orElseThrow());
     }
 
-    /*
-     * @MessageMapping("/group/{groupId}")
-     * public void testGroup(@DestinationVariable String groupId, Message
-     * messageReceived) {
-     * System.out.println("Message for group: " + groupId + ", received!");
-     * 
-     * messagingTemplate.convertAndSend("topic/group/" + groupId, new Message());
-     * }
-     */
 }
