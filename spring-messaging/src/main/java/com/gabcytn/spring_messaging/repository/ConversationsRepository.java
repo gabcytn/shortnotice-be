eefaa1930d39aba06813e07ec4a5932050cf10ce
gabcytn <cayetanogabriel03@gmail.com>
2024-12-31 23:37:25 +0800

    Feat: save and add members to conversations
    Order of commit is incorrect: last 2 should interchange

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/repository/ConversationsRepository.java
@@ -0,0 +1,36 @@
+package com.gabcytn.spring_messaging.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class ConversationsRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ConversationsRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Integer create (boolean isRequest) {
+        final String sqlQuery = """
+                INSERT INTO conversations (request)
+                VALUES (?)
+                RETURNING id
+                """;
+        final RowMapper<Integer> idRowMapper = (rs, rowNum) -> rs.getInt("id");
+
+        return jdbcTemplate.queryForObject(sqlQuery, idRowMapper, isRequest);
+    }
+
+    public void saveMembers(int conversationId, UUID user1, UUID user2) {
+        final String sqlQuery = """
+                INSERT INTO conversation_members (conversation_id, user_id)
+                VALUES (?, ?), (?, ?)
+                """;
+        jdbcTemplate.update(sqlQuery, conversationId, user1, conversationId, user2);
+    }
+}
