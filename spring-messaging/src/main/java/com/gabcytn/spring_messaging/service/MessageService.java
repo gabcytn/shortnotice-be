5b6fb0dc2ace76ca7a8924947122b1bb3de9c6ca
gabcytn <cayetanogabriel03@gmail.com>
2025-01-01 15:06:49 +0800

    Feat: check existing conversation, accept message requests, and save messages in db

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/service/MessageService.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/service/MessageService.java
@@ -3,6 +3,7 @@ package com.gabcytn.spring_messaging.service;
 import com.gabcytn.spring_messaging.model.Message;
 import com.gabcytn.spring_messaging.model.PrivateMessage;
 import com.gabcytn.spring_messaging.repository.ConversationsRepository;
+import com.gabcytn.spring_messaging.repository.MessageRepository;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,14 @@ import java.util.UUID;
 @Service
 public class MessageService {
     private final ConversationsRepository conversationsRepository;
+    private final MessageRepository messageRepository;
 
     public MessageService(
-            ConversationsRepository conversationsRepository
+            ConversationsRepository conversationsRepository,
+            MessageRepository messageRepository
     ) {
         this.conversationsRepository = conversationsRepository;
+        this.messageRepository = messageRepository;
     }
 
     public Optional<PrivateMessage> createMessageRequest (
@@ -32,8 +36,11 @@ public class MessageService {
             final String content = messageReceived.content();
             final Timestamp currentTimestamp = Timestamp.valueOf(LocalDateTime.now());
 
-            final int conversationId = conversationsRepository.create(true);
+            if (isMessageRequestExisting(senderUUID, recipientUUID)) return Optional.empty();
+
+            final int conversationId = conversationsRepository.create();
             conversationsRepository.saveMembers(conversationId, senderUUID, recipientUUID);
+            messageRepository.save(conversationId, senderUUID, content);
 
             return Optional.of(new PrivateMessage(senderUsername, content, currentTimestamp));
         } catch (Exception e) {
@@ -41,6 +48,32 @@ public class MessageService {
             System.err.println(e.getMessage());
             return Optional.empty();
         }
+    }
+
+    public Optional<PrivateMessage> acceptMessageRequest (
+            SimpMessageHeaderAccessor headerAccessor,
+            Message messageReceived,
+            int conversationId
+    ) {
+        try {
+            final UUID senderUUID = getMessageSenderUUID(headerAccessor);
+            final String senderUsername = getMessageSenderUsername(headerAccessor);
+            final String content = messageReceived.content();
+            final Timestamp currentTimestamp = Timestamp.valueOf(LocalDateTime.now());
+
+            conversationsRepository.setRequestFalseById(conversationId);
+            messageRepository.save(conversationId, senderUUID, content);
+
+            return Optional.of(new PrivateMessage(senderUsername, content, currentTimestamp));
+        } catch (Exception e) {
+            System.err.println("Error accepting message request");
+            System.err.println(e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    private Boolean isMessageRequestExisting (UUID sender, UUID recipient) {
+        return conversationsRepository.isConversationExisting(sender, recipient);
     }
 
     private UUID getMessageSenderUUID(SimpMessageHeaderAccessor headerAccessor) {
