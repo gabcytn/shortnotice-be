0bc98148d234c360da2ee1cd26ad4f25e095fb19
gabcytn <cayetanogabriel03@gmail.com>
2025-01-02 09:49:26 +0800

    Refactor: remove unnecessary imports

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/repository/ConversationsRepository.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/repository/ConversationsRepository.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
