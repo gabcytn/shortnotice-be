6dde6bbeb9aa982c566280a9be91f0c6a2cab688
gabcytn <cayetanogabriel03@gmail.com>
2025-01-03 08:32:54 +0800

    Refactor: add necessary properties

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/model/PrivateMessage.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/model/PrivateMessage.java
@@ -5,13 +5,17 @@ import java.sql.Timestamp;
 public class PrivateMessage {
     private String sender;
     private String message;
+    private int conversationId;
+    private boolean isRequest;
     private Timestamp sentAt;
 
     public PrivateMessage() {}
 
-    public PrivateMessage(String sender, String message, Timestamp sentAt) {
+    public PrivateMessage(String sender, String message, int conversationId, boolean isRequest, Timestamp sentAt) {
         this.sender = sender;
         this.message = message;
+        this.conversationId = conversationId;
+        this.isRequest = isRequest;
         this.sentAt = sentAt;
     }
 
@@ -39,11 +43,29 @@ public class PrivateMessage {
         this.sentAt = sentAt;
     }
 
+    public int getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(int conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public boolean isRequest() {
+        return isRequest;
+    }
+
+    public void setRequest(boolean request) {
+        isRequest = request;
+    }
+
     @Override
     public String toString() {
-        return "PrivateMessageView{" +
+        return "PrivateMessage{" +
                 "sender='" + sender + '\'' +
                 ", message='" + message + '\'' +
+                ", conversationId=" + conversationId +
+                ", isRequest=" + isRequest +
                 ", sentAt=" + sentAt +
                 '}';
     }
