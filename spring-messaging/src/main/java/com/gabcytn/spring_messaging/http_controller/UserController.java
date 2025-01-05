d944913e19835b978c7d9b394b5b9405187b9bd1
gabcytn <cayetanogabriel03@gmail.com>
2025-01-05 19:50:11 +0800

    Feat: add endpoint to search for users by username

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/http_controller/UserController.java
@@ -0,0 +1,26 @@
+package com.gabcytn.spring_messaging.http_controller;
+
+import com.gabcytn.spring_messaging.model.User;
+import com.gabcytn.spring_messaging.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<User>> searchByUsername (@RequestParam(value = "username") String username) {
+        return userService.searchByUsername(username);
+    }
+}
