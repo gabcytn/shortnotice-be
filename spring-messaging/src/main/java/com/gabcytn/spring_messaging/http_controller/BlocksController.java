3951c4a2220adfe9fe822e72bbdf8c2ad3f5bd04
gabcytn <cayetanogabriel03@gmail.com>
2025-01-05 18:55:30 +0800

    Feat: expose an endpoint to block a user

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/http_controller/BlocksController.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/http_controller/BlocksController.java
@@ -4,11 +4,10 @@ import com.gabcytn.spring_messaging.model.User;
 import com.gabcytn.spring_messaging.service.BlocksService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/block")
@@ -22,5 +21,10 @@ public class BlocksController {
     @GetMapping("/list")
     public ResponseEntity<List<User>> getBlocksList (HttpServletRequest request) {
         return blocksService.getBlocksList(request);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> blockById (@PathVariable UUID id, HttpServletRequest request) {
+        return blocksService.blockById(request, id);
     }
 }
