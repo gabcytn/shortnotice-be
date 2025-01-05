5a1a6f6cb95537bff428bd8d3ee6c861599fd874
gabcytn <cayetanogabriel03@gmail.com>
2025-01-05 18:22:02 +0800

    Feat: profilePic property

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/model/User.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/model/User.java
@@ -6,14 +6,16 @@ public class User {
     private UUID id = UUID.randomUUID();
     private String username;
     private String password;
+    private String profilePic;
     private String role;
 
     public User () {}
 
-    public User(UUID id, String username, String password, String role) {
+    public User(UUID id, String username, String password, String profilePic, String role) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.profilePic = profilePic;
         this.role = role;
     }
 
@@ -41,6 +43,14 @@ public class User {
         this.password = password;
     }
 
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
     public String getRole() {
         return role;
     }
@@ -55,6 +65,7 @@ public class User {
                 "id=" + id +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", profilePic='" + profilePic + '\'' +
                 ", role='" + role + '\'' +
                 '}';
     }
