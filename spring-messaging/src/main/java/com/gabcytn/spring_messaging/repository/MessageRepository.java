d3a9036361d2d294d7d0e428c21d37a934b63082
gabcytn <cayetanogabriel03@gmail.com>
2025-01-06 18:57:47 +0800

    Feat: find messages in a conversation (query)

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/repository/MessageRepository.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/repository/MessageRepository.java
@@ -1,8 +1,12 @@
 package com.gabcytn.spring_messaging.repository;
 
+import com.gabcytn.spring_messaging.model.PrivateMessage;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -19,5 +23,29 @@ public class MessageRepository {
                 VALUES (?, ?, ?)
                 """;
         jdbcTemplate.update(sqlQuery, conversationId, sender, message);
+    }
+
+    public List<PrivateMessage> findAllByConversationId (int conversationId) {
+        final String sqlQuery = """
+                SELECT
+                    conversation_id, users.username AS sender, message, sent_at
+                FROM
+                    messages
+                JOIN
+                    users
+                ON
+                    users.id = messages.sender_id
+                WHERE
+                    conversation_id = ?
+                """;
+        final RowMapper<PrivateMessage> privateMessageRowMapper = (rs, rowNum) -> new PrivateMessage(
+            rs.getInt("conversation_id"),
+            rs.getString("sender"),
+            rs.getString("message"),
+            false,
+            rs.getTimestamp("sent_at")
+        );
+
+        return jdbcTemplate.query(sqlQuery, privateMessageRowMapper, conversationId);
     }
 }
