e44d216f7bae2eacb9c5bc46b3425ef8e589428e
gabcytn <cayetanogabriel03@gmail.com>
2025-01-06 18:58:37 +0800

    Feat: add endpoint for retrieving message history in a conversation

---
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/http_controller/HttpMessageController.java
@@ -0,0 +1,26 @@
+package com.gabcytn.spring_messaging.http_controller;
+
+import com.gabcytn.spring_messaging.model.PrivateMessage;
+import com.gabcytn.spring_messaging.service.MessageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/message")
+public class HttpMessageController {
+    private final MessageService messageService;
+
+    public HttpMessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/history/{conversationId}")
+    public ResponseEntity<List<PrivateMessage>> getMessageHistory (@PathVariable int conversationId) {
+        return messageService.getMessageHistory(conversationId);
+    }
+}
