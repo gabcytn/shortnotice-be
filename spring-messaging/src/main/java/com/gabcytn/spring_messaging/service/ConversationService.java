1ed2c5704908a1a27f7c5bc939a2825823d43982
gabcytn <cayetanogabriel03@gmail.com>
2025-01-08 22:14:47 +0800

    Service: process request for conversation id

--- spring-messaging/src/main/java/com/gabcytn/spring_messaging/service/ConversationService.java
+++ spring-messaging/src/main/java/com/gabcytn/spring_messaging/service/ConversationService.java
@@ -30,4 +30,18 @@ public class ConversationService {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    public Integer getConversationId (HttpServletRequest request, UUID id) {
+        try {
+            final UUID requesterId = UUID.fromString((String) request.getSession().getAttribute("uuid"));
+            final Integer conversationId = conversationsRepository.existsByMembers(requesterId, id);
+            if (conversationId != null && conversationId > 0)
+                return conversationId;
+            throw new Error("Conversation id is either null or negative");
+        } catch (Exception e) {
+            System.err.println("Error getting conversation id");
+            System.err.println(e.getMessage());
+            return -1;
+        }
+    }
 }
