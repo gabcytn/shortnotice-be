e0fcadc0a5b68e4d0f1f74400caad7bbe4b6e5ab
gabcytn <cayetanogabriel03@gmail.com>
2025-01-09 14:18:43 +0800

    Uncomment home page

--- expo-messaging/app/_layout.tsx
+++ expo-messaging/app/_layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout() {
     >
       <Stack.Screen name="index" />
       <Stack.Screen name="auth" />
-      {/* <Stack.Screen name="home" /> */}
+      <Stack.Screen name="home" />
     </Stack>
   );
 }
