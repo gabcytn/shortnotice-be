8a5980e575815fdbb5573624c2ee06a0d73a8b28
gabcytn <cayetanogabriel03@gmail.com>
2025-01-09 13:50:56 +0800

    Style: define root layout of pages and load font

--- expo-messaging/app/_layout.tsx
+++ expo-messaging/app/_layout.tsx
@@ -1,5 +1,31 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    Poppins: require("@/assets/fonts/Poppins.ttf"),
+    PoppinsBold: require("@/assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
+
+  if (!loaded) return null;
+  return (
+    <Stack
+      screenOptions={{
+        statusBarTranslucent: true,
+        headerShown: false,
+        statusBarStyle: "dark",
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="auth" />
+      {/* <Stack.Screen name="home" /> */}
+    </Stack>
+  );
 }
