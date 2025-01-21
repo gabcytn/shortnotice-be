15ced8ac2ad36d6fbdf36d1b022bad4ff60fa80f
gabcytn <cayetanogabriel03@gmail.com>
2025-01-21 09:57:54 +0800

    Refactor: display conversations

--- expo-messaging/app/home/index.tsx
+++ expo-messaging/app/home/index.tsx
@@ -1,9 +1,12 @@
 import {
+  Keyboard,
   Platform,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
+  View,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { logout } from "../service/auth";
@@ -11,14 +14,16 @@ import { startup } from "../service/home";
 import { Colors } from "@/constants/Colors";
 import Button from "@/components/Button";
 import InputBox from "@/components/InputText";
+import { Conversation } from "../types/home";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [search, setSearch] = useState("");
+  const [conversations, setConversations] = useState<Conversation[]>([]);
 
   useEffect(() => {
     async function effect() {
-      await startup(setIsLoading);
+      await startup(setIsLoading, setConversations);
     }
     effect();
   }, []);
@@ -26,22 +31,33 @@ const Home = () => {
   if (isLoading) return <Text>LOADING...</Text>;
 
   return (
-    <ScrollView style={styles.container}>
-      <InputBox
-        placeholder="Search"
-        isSecure={false}
-        value={search}
-        setValue={setSearch}
-        styles={styles}
-      />
-      <Button
-        title="Logout"
-        onPress={logout}
-        styles={styles}
-        disabled={isLoading}
-        opacity={0.7}
-      />
-    </ScrollView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <ScrollView style={styles.container}>
+        <InputBox
+          placeholder="Search"
+          isSecure={false}
+          value={search}
+          setValue={setSearch}
+          styles={styles}
+        />
+        {conversations.map((conversation) => {
+          return (
+            <View key={conversation.id}>
+              <Text>{conversation.senderUsername}</Text>
+              <Text>{conversation.message}</Text>
+              <Text>{conversation.sentAt}</Text>
+            </View>
+          );
+        })}
+        <Button
+          title="Logout"
+          onPress={logout}
+          styles={styles}
+          disabled={isLoading}
+          opacity={0.7}
+        />
+      </ScrollView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -57,7 +73,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     borderWidth: 1,
     borderRadius: 5,
-    width: 250,
+    width: "100%",
     paddingHorizontal: 5,
     paddingVertical: 3,
     marginVertical: 5,
