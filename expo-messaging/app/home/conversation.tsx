81a632b4de93fa86c59bdf2ee5e6934608c06046
gabcytn <cayetanogabriel03@gmail.com>
2025-01-26 10:17:09 +0800

    Feat: chat interface

--- expo-messaging/app/home/conversation.tsx
+++ expo-messaging/app/home/conversation.tsx
@@ -1,18 +1,43 @@
-import {
-  Platform,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-} from "react-native";
-import React from "react";
+import { Alert, Platform, StatusBar, StyleSheet } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
 import { Colors } from "@/constants/Colors";
+import { GiftedChat, IMessage } from "react-native-gifted-chat";
+import { useLocalSearchParams } from "expo-router";
+import { fetchMessages } from "../service/conversation";
+
+type Params = {
+  id: string;
+  username: string;
+  avatar: string;
+};
 
 const Conversation = () => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const params = useLocalSearchParams<Params>();
+  useEffect(() => {
+    const fetchPreviousMessages = async () => {
+      const messages = await fetchMessages(parseInt(params.id), params.avatar);
+      if (!messages) {
+        Alert.alert("NO MESSAGES / ERROR");
+        return;
+      }
+
+      setMessages(messages);
+    };
+
+    fetchPreviousMessages();
+  }, []);
+  const onSend = useCallback((messages: IMessage[] = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
   return (
-    <ScrollView style={styles.container}>
-      <Text>Conversation</Text>
-    </ScrollView>
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{ _id: "user1" }}
+    />
   );
 };
 
