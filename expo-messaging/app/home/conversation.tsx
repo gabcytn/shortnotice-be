b48bc96f2164550721a1d4ff81eeec17b1b01186
gabcytn <cayetanogabriel03@gmail.com>
2025-01-27 00:04:33 +0800

    Feat: send and receive normal messages

--- expo-messaging/app/home/conversation.tsx
+++ expo-messaging/app/home/conversation.tsx
@@ -5,18 +5,32 @@ import { GiftedChat, IMessage } from "react-native-gifted-chat";
 import { useLocalSearchParams } from "expo-router";
 import { fetchMessages } from "../service/conversation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Params } from "../types/conversation";
+import { IncomingMessage, Params } from "../types/conversation";
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+
+const SOCKET_URL = "http://192.168.68.106:8080/short-notice"; // Replace with your server URL
+
+const client = new Client({
+  webSocketFactory: () => new SockJS(SOCKET_URL),
+  debug: (str) => {
+    console.log("STOMP Debug:", str);
+  },
+});
 
 const Conversation = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
-  const [userId, setUserId] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-
   const params = useLocalSearchParams<Params>();
+
   useEffect(() => {
     const fetchPreviousMessages = async () => {
       setIsLoading(true);
-      const messages = await fetchMessages(parseInt(params.id), params.avatar);
+      const messages = await fetchMessages(
+        parseInt(params.convoId),
+        params.convoAvatar,
+      );
 
       if (!messages) {
         Alert.alert("NO MESSAGES / ERROR");
@@ -24,26 +38,54 @@ const Conversation = () => {
       }
 
       setMessages(messages);
-      const id = await AsyncStorage.getItem("username");
-      setUserId(id);
+      const id = await AsyncStorage.getItem("id");
+      const username = await AsyncStorage.getItem("username");
+
+      client.onConnect = () => {
+        client.subscribe(`/topic/private/${username}`, (message) => {
+          const socketResponse = message.body.body;
+          console.log(socketResponse);
+          Alert.alert("MESSAGE RECEIVED: ", message.body);
+          console.log(message);
+        });
+      };
+
+      client.activate();
+
+      setUsername(username);
       setIsLoading(false);
     };
 
     fetchPreviousMessages();
-  }, []);
-  const onSend = useCallback((messages: IMessage[] = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages),
-    );
+    return () => {
+      client.deactivate();
+    };
   }, []);
 
-  return isLoading || !userId ? (
+  const onSend = (messages: IMessage[] = []) => {
+    if (client && client.connected) {
+      client.publish({
+        destination: `/app/private/${params.convoId}`,
+        body: JSON.stringify({
+          content: messages[0].text,
+          recipient: params.convoUsername,
+        }),
+      });
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages),
+      );
+    } else {
+      console.error("Stomp client is not connected");
+    }
+  };
+
+  return isLoading || !username ? (
     <Text>LOADING...</Text>
   ) : (
     <GiftedChat
       messages={messages}
       onSend={(messages) => onSend(messages)}
-      user={{ _id: userId }}
+      user={{ _id: username }}
     />
   );
 };
