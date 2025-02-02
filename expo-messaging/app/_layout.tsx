89c6262e53b8f7383975a74b11cad05412eb7e91
gabcytn <cayetanogabriel03@gmail.com>
2025-02-02 12:12:28 +0800

    Refactor: wrap app in query client provider

--- expo-messaging/app/_layout.tsx
+++ expo-messaging/app/_layout.tsx
@@ -1,8 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -16,16 +18,18 @@ export default function RootLayout() {
 
   if (!loaded) return null;
   return (
-    <Stack
-      screenOptions={{
-        statusBarTranslucent: true,
-        headerShown: false,
-        statusBarStyle: "dark",
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="auth" />
-      <Stack.Screen name="home" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          statusBarTranslucent: true,
+          headerShown: false,
+          statusBarStyle: "dark",
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="auth" />
+        <Stack.Screen name="home" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
