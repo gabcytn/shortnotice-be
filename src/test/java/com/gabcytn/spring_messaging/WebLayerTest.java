0923fb66c0ec9e67af2129cb04105636eb8c3ac3
gabcytn <cayetanogabriel03@gmail.com>
2025-03-07 20:09:41 +0800

    test: security

--- src/test/java/com/gabcytn/spring_messaging/WebLayerTest.java
+++ src/test/java/com/gabcytn/spring_messaging/WebLayerTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -23,5 +25,30 @@ public class WebLayerTest {
     public void mustAuthenticateFirst () throws Exception{
         this.mockMvc.perform(get("/conversation/list")).andDo(print())
                 .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void authStatusMustReturnFalseOnUnauthenticatedRequest () throws Exception
+    {
+        mockMvc.perform(get("/auth/status"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"));
+    }
+
+    @Test
+    public void login() throws Exception
+    {
+        User user = new User();
+        user.setUsername("user1");
+        user.setPassword("pass1");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk());
     }
 }
